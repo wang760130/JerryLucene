117ceab5a2ae3d2406a0dd74399a1e6f47fc7f94
JerryWang <jerry002@126.com>
2016-09-16 16:44:54 +0800

    HelloIndex

--- src/main/java/com/jerry/lucene/hello/HelloIndex.java
+++ src/main/java/com/jerry/lucene/hello/HelloIndex.java
@@ -38,6 +38,7 @@ public class HelloIndex {
 			"I like football and I like basketball too",
 			"I like movie and swim"
 	};
+	
 	private static final Date[] dates = null;
 	private static final int[] attachs = {2,3,1,4,5,5};
 	private static final String[] names = {"zhangsan","lisi","john","jetty","mike","jake"};
@@ -57,16 +58,13 @@ public class HelloIndex {
 		try {
 			IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35, new StandardAnalyzer(Version.LUCENE_35)));
 		
-			indexWriter.deleteAll();
-			
 			Document document = new Document();
 			
 			for(int i = 0; i < ids.length; i++) {
 				document.add(new Field("id", ids[i], Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS));
-				document.add(new Field("id","11",Field.Store.YES,Field.Index.NOT_ANALYZED_NO_NORMS));
-				document.add(new Field("email",emails[0],Field.Store.YES,Field.Index.NOT_ANALYZED));
-				document.add(new Field("content",contents[0],Field.Store.NO,Field.Index.ANALYZED));
-				document.add(new Field("name",names[0],Field.Store.YES,Field.Index.NOT_ANALYZED_NO_NORMS));
+				document.add(new Field("email",emails[i],Field.Store.YES,Field.Index.NOT_ANALYZED));
+				document.add(new Field("content",contents[i],Field.Store.NO,Field.Index.ANALYZED));
+				document.add(new Field("name",names[i],Field.Store.YES,Field.Index.NOT_ANALYZED_NO_NORMS));
 				indexWriter.addDocument(document);
 			}
 			
@@ -80,21 +78,117 @@ public class HelloIndex {
 		}
 	}
 	
-	public static void query() {
+	public static void deleteAll() {
 		try {
-			IndexReader indexReader = IndexReader.open(directory);
-			System.out.println("numDocs:" + indexReader.numDocs());
-			System.out.println("maxDocs:" + indexReader.maxDoc());
-			
+			IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35, new StandardAnalyzer(Version.LUCENE_35)));
+			indexWriter.deleteAll();
+			indexWriter.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (LockObtainFailedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void delete() {
+		try {
+			IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35, new StandardAnalyzer(Version.LUCENE_35)));
+			// 参数是一个选项，可以是一个Query，也可以是一个term，term是一个精确查找的值
+			// 此时删除的文档并不会被完全删除，而是存储在一个回收站中的，可以恢复
+			indexWriter.deleteDocuments(new Term("id", "1"));
+			indexWriter.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (LockObtainFailedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void undelete() {
+		// 使用IndexReader进行恢复
+		try {
+			// 恢复时， 必须把IndexReader的只读（readOnly）设置为false
+			IndexReader indexReader = IndexReader.open(directory, false);
+			indexReader.undeleteAll();
+			indexReader.clone();
 		} catch (CorruptIndexException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+	}
+	
+	public static void forceMergeDeletes() {
+		// 使用IndexReader进行恢复
+		try {
+			// 恢复时， 必须把IndexReader的只读（readOnly）设置为false
+			IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35,new StandardAnalyzer(Version.LUCENE_35)));
+			indexWriter.forceMergeDeletes();
+			indexWriter.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void forceMerge() {
+		try {
+			IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35,new StandardAnalyzer(Version.LUCENE_35)));
+			// 会将索引合并为两断，这两段中的被删除的数据会被清空
+			// 特别注意：此处Lucene在3.5之后不建议使用，因为会消耗大量的开销，Luceue会根据情况自动处理
+			indexWriter.forceMerge(2);
+			indexWriter.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void update() {
+		try {
+			IndexWriter indexWriter = new IndexWriter(directory, new IndexWriterConfig(Version.LUCENE_35,new StandardAnalyzer(Version.LUCENE_35)));
+			
+			// Luceue并没有提供更新，这里的更新操作其实是如下两个操作的集合
+			// 先删除之后在添加
+			
+			Document document = new Document();
+			document.add(new Field("id", "1", Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS));
+			document.add(new Field("email",emails[0],Field.Store.YES,Field.Index.NOT_ANALYZED));
+			document.add(new Field("content",contents[0],Field.Store.NO,Field.Index.ANALYZED));
+			document.add(new Field("name",names[0],Field.Store.YES,Field.Index.NOT_ANALYZED_NO_NORMS));
+			indexWriter.updateDocument(new Term("id","1"), document);
+			
+			indexWriter.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void query() {
+		try {
+			IndexReader indexReader = IndexReader.open(directory);
+			// 通过reader可以有效的获取到文档的数量
+			System.out.println("numDocs:" + indexReader.numDocs());
+			System.out.println("maxDocs:" + indexReader.maxDoc());
+			System.out.println("deleteDocs:" + indexReader.numDeletedDocs());
+			indexReader.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
+		HelloIndex.deleteAll();
 		HelloIndex.index();
 		HelloIndex.query();
 	}
