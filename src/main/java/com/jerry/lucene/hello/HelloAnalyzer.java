33011cb62d09fb30a0f0da01ea39c572920960ca
JerryWang <jerry002@126.com>
2016-09-27 22:17:47 +0800

    HelloAnalyzer

--- src/main/java/com/jerry/lucene/hello/HelloAnalyzer.java
+++ src/main/java/com/jerry/lucene/hello/HelloAnalyzer.java
@@ -33,9 +33,13 @@ public class HelloAnalyzer {
 	public static void displayAllToTokenInfo(String fieldName, String str, Analyzer analyzer) {
 		try {
 			TokenStream stream = analyzer.tokenStream(fieldName, new StringReader(str));
+			// 位置增量的属性，存储词汇单元之间的距离
 			PositionIncrementAttribute pia = stream.addAttribute(PositionIncrementAttribute.class);
+			// 每个词汇单元的位置偏移量
 			OffsetAttribute oa = stream.addAttribute(OffsetAttribute.class);
+			// 存储每一个词汇单元的信息（分词单元信息）
 			CharTermAttribute cta = stream.addAttribute(CharTermAttribute.class);
+			// 使用的分词器的类型信息
 			TypeAttribute ta = stream.addAttribute(TypeAttribute.class);
 			
 			for(;stream.incrementToken();) {
