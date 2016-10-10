f6b0ab3da17bc5acf6b387e38a3484c248f608db
JerryWang <jerry002@126.com>
2016-10-10 22:27:14 +0800

    lucene

--- src/main/java/com/jerry/lucene/searcher/HelloSearcher.java
+++ src/main/java/com/jerry/lucene/searcher/HelloSearcher.java
@@ -2,11 +2,13 @@ package com.jerry.lucene.searcher;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -16,6 +18,7 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.BooleanClause.Occur;
@@ -33,10 +36,22 @@ import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TermRangeQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.WildcardQuery;
+import org.apache.lucene.search.highlight.Formatter;
+import org.apache.lucene.search.highlight.Fragmenter;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
+import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
+import org.apache.lucene.search.similar.MoreLikeThis;
+import org.apache.lucene.search.vectorhighlight.FastVectorHighlighter;
+import org.apache.lucene.search.vectorhighlight.FieldQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.util.Version;
+
+import com.chenlb.mmseg4j.analysis.MMSegAnalyzer;
 
 /**
  * @author Jerry Wang
@@ -471,6 +486,98 @@ public class HelloSearcher {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void searcherByHighlighter(String query) {
+		try {
+			Analyzer analyzer = new MMSegAnalyzer();
+			IndexSearcher searcher = getSearcher();
+			MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_35, new String[]{"title","content"}, analyzer);
+			Query q = parser.parse(query);
+			TopDocs tds = searcher.search(q, 20);
+			MoreLikeThis mlt = new MoreLikeThis(searcher.getIndexReader());
+			mlt.setFieldNames(new String[]{"title","content"});
+			mlt.setMinDocFreq(1);
+			mlt.setMinTermFreq(1);
+			for(ScoreDoc sd : tds.scoreDocs) {
+				Document doc = searcher.doc(sd.doc);
+				String title = doc.get("title");
+				title = lighterStr(analyzer, q, title, "title");
+				System.out.println(title);
+				Query moreLike = mlt.like(sd.doc);
+				TopDocs stds = searcher.search(moreLike, 10);
+				for(ScoreDoc ssd:stds.scoreDocs) {
+					Document d = searcher.doc(ssd.doc);
+					System.out.println(d.get("title"));
+				}
+			}
+			searcher.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (InvalidTokenOffsetsException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void searcherByFastHighlighter(String name) {
+		try {
+			FastVectorHighlighter fvh = new FastVectorHighlighter(false,false);
+			Analyzer a = new MMSegAnalyzer();
+			IndexSearcher searcher = getSearcher();
+			MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_35, new String[]{"title","content"}, a);
+			Query query = parser.parse(name);
+			FieldQuery fq = fvh.getFieldQuery(query);
+			TopDocs tds = searcher.search(query, 20);
+			for(ScoreDoc sd:tds.scoreDocs) {
+				String highTitle = fvh.getBestFragment(fq, searcher.getIndexReader(), sd.doc, "title", 100);
+				System.out.println(highTitle);
+				String highContent = fvh.getBestFragment(fq,  searcher.getIndexReader(), sd.doc, "content",100);
+				System.out.println(highContent);
+			}
+			searcher.close();
+		} catch (CorruptIndexException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	private String lighterStr(Analyzer a, Query query, String txt, String fieldname) throws IOException, InvalidTokenOffsetsException {
+		QueryScorer scorer = new QueryScorer(query);
+		Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);
+		Formatter fmt = new SimpleHTMLFormatter("<b>", "</b>");
+		Highlighter lighter = new Highlighter(fmt, scorer);
+		lighter.setTextFragmenter(fragmenter);
+		String str = lighter.getBestFragments(a.tokenStream(fieldname,new StringReader(txt)),txt, 3, "......\n");
+		if(str == null)
+			return txt;
+		return str;
+	}
+	
+	public void lighter01() {
+		try {
+			String txt = "我爱北京天安门，天安门上彩旗飞,伟大领袖毛主席，指引我们向前进，向前进！！！想起身离开东京法律思考的机会那个上的讲话那伟大的个圣诞sadfsadnfl.sajdfl;aksjdf;lsadfsadfm.asd那是肯定激发了深刻的机会拉萨宽带计费了那个傻大姐华纳公司的机会节贺卡就是对话框那是国天安门际  北京电话卡开始觉啊 北京得人们大会堂  北京！！！！";
+			Query query = new QueryParser(Version.LUCENE_35, "f", new MMSegAnalyzer()).parse("北京 伟大");
+			QueryScorer scorer = new QueryScorer(query);
+			Fragmenter fragment = new SimpleSpanFragmenter(scorer);
+			Formatter formatter = new SimpleHTMLFormatter("<span style='color:red'>", "</span>");
+			Highlighter highlighter = new Highlighter(formatter,scorer);
+			highlighter.setTextFragmenter(fragment);
+			String str = highlighter.getBestFragment(new MMSegAnalyzer(), "f", txt);
+			System.out.println(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InvalidTokenOffsetsException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 	}
